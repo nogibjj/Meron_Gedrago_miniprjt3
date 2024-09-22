84f0e6775ae61f09a406098c826978abe9b78a0c
Gedrago <mgedrago18@amherst.edu>
2024-09-22 01:02:41 -0400

    first commit

--- mylib/lib.py
+++ mylib/lib.py
@@ -1,26 +1,26 @@
 # import packages
-import pandas as pd
+import polars as pl
 import matplotlib.pyplot as plt
 
 
 # create a function to read the dataset and filter the
 def load_dataset(dataset):
-    data_set = pd.read_csv(dataset)
+    data_set = pl.read_csv(dataset)
     return data_set
 
 
 # calculate and print the summary statistics
 def describe_data(input_data):
-    mean = input_data.mean()
-    median = input_data.median()
-    std = input_data.std()
+    mean = pl.mean(input_data)
+    median = pl.median(input_data)
+    std = pl.std(input_data)
     return f"The mean is {mean}; the median is {median}; the sd is {std}"
 
 
 # create a function to get the median of the data
 def find_min_and_max(input_data):
-    data_max = input_data.max()
-    data_min = input_data.min()
+    data_max = pl.max(input_data)
+    data_min = pl.min(input_data)
     return f"The max is {data_max} and the min is {data_min}"
 
 
